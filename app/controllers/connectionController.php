dbec95feb33231ef318ad91f5d693a34f7842365
Juan Diego Bermudez Castañeda <juan052115@gmail.com>
2023-06-16 16:41:25 -0500

    The connectionController.php file is added, which will be responsible for receiving all client requests and forwarding them to the models to obtain the requested information or perform the necessary actions.

---
+++ app/controllers/connectionController.php
@@ -0,0 +1,15 @@
+<?php
+include_once("../models/connectionModel.php");
+
+//This class enables communication between the view (client) and the model. In other words, this class will be responsible for creating the database connection and handling client requests related to it.
+class ConnectionController
+{
+  private $connectionModel; 
+
+  //This method allows creating the instance of the database.
+  public function __construct($host, $dbname, $username, $password)
+  {
+    $this->connectionModel = ConnectionModel::getInstance($host, $dbname, $username, $password); 
+  }
+
+}
